4e3cf77c064175346fd03352a3b4cfa1feee0fe2
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-06 18:12:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,6 +19,14 @@ class Song
     @@all
   end
   
+  def artist_name=
+  end
+  
+  
+  
+  
+  
+  
   def self.new_by_filename(file)
     song_data = file.chomp("mp3").split(" - ")
     song = Song.new(song_data[1])
