8a9961763791b391ceb033c67dbeb8a2f6528a5b
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-06 18:27:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,14 +19,10 @@ class Song
     @@all
   end
   
-  
-  
-  
-  
-  
-  
-  
-  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    Artist.add_song(self)
+  end
   
   def self.new_by_filename(file)
     song_data = file.chomp("mp3").split(" - ")
@@ -35,10 +31,6 @@ class Song
     song 
   end
   
-   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    Artist.add_song(self)
-  end
   
   
   
